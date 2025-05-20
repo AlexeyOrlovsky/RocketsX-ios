b33a44f157966396f6598ac19f17674c31a725a1
AlexeyOrlovsky <orlovskyiosdev@gmail.com>
2025-05-21 01:18:38 +0300

    feature: initial setup project

---
+++ RocketsX/App/RocketsXApp.swift
@@ -0,0 +1,17 @@
+//
+//  RocketsXApp.swift
+//  RocketsX
+//
+//  Created by Алексей Орловский on 21.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct RocketsXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
