f6b2fa73f21fb3b9e923ece75924c4cd8d8fd468
AlexeyOrlovsky <orlovskyiosdev@gmail.com>
2025-05-21 21:33:23 +0300

    feature: integrated firebase

--- RocketsX/App/RocketsXApp.swift
+++ RocketsX/App/RocketsXApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct RocketsXApp: App {
+    // MARK: - Init
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
